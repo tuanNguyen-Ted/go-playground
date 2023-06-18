79cd19b74db24408fd75d169c1ec5e070d935c58
TedNguyen <ted.nguyen12@gmail.com>
2023-06-18 13:37:11 +0700

    init ginc component

---
+++ micro-clean-arhchitecture-service-demo/component/ginc/gin.go
@@ -0,0 +1,68 @@
+package ginc
+
+import (
+	"demo-service/common"
+	"flag"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	defaultPort = 3000
+	defaultMode = "debug"
+)
+
+type Config struct {
+	port    int
+	ginMode string
+}
+
+type ginEngine struct {
+	*Config
+	name   string
+	id     string
+	logger common.Logger
+	router *gin.Engine
+}
+
+func NewGin(id string) *ginEngine {
+	return &ginEngine{
+		Config: new(Config),
+		id:     id,
+	}
+}
+
+func (gs *ginEngine) ID() string {
+	return gs.id
+}
+
+func (gs *ginEngine) Activate(sv common.ServiceContext) error {
+	gs.logger = sv.Logger(gs.id)
+	gs.name = sv.GetName()
+
+	if gs.ginMode == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	gs.logger.Info("init engine...")
+	gs.router = gin.New()
+
+	return nil
+}
+
+func (gs *ginEngine) Stop() error {
+	return nil
+}
+
+func (gs *ginEngine) InitFlags() {
+	flag.IntVar(&gs.Config.port, "gin-port", defaultPort, "gin server port. Default 3000")
+	flag.StringVar(&gs.Config.ginMode, "gin-mode", defaultMode, "gin mode (debug | release). Default debug")
+}
+
+func (gs *ginEngine) GetPort() int {
+	return gs.port
+}
+
+func (gs *ginEngine) GetRouter() *gin.Engine {
+	return gs.router
+
+}
